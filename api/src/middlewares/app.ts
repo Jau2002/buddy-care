4ee6c62f2ea80516d02e6c3d88cbf3fee50d06db
Jau2002 <idamendjaume@gmail.com>
2022-12-07 02:34:41 -0500

    fix: tranpilation TS to JS server

--- api/src/middlewares/app.ts
+++ api/src/middlewares/app.ts
@@ -1,7 +1,19 @@
-const express = require('express');
+import express from 'express';
+import morganBody from 'morgan-body';
+import rootRoute from '../routes';
 
 const app = express();
 
 app.use(express.json({ limit: '50mb' }));
 
-module.exports = app;
+morganBody(app, {
+	theme: 'inverted',
+	logReqDateTime: false,
+	logIP: false,
+	logReqUserAgent: false,
+	immediateReqLog: false,
+});
+
+app.use('/', rootRoute);
+
+export default app;
