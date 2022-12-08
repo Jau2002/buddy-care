4ebceb99b3f4b22c73449e1be1839a4bc0f6f00d
Tbownt <98657140+Tbownt@users.noreply.github.com>
2022-12-08 17:13:30 -0400

    refactor(getHealths)
    Hola a todos

--- api/src/controllers/get/health.ts
+++ api/src/controllers/get/health.ts
@@ -1,13 +1,13 @@
 import { NOT_FOUND, OK } from '../protocols';
-import {Request, Response} from 'express'
+import { Request, Response } from 'express';
 
-const getHealth = async (req: Request, res: Response):Promise<unknown> => {
-	console.log(req)
+const getHealth = async (req: Request, res: Response): Promise<unknown> => {
+	console.log(req);
 	try {
 		return res.status(OK).send({ message: 'the server is running' });
 	} catch (err) {
 		return res.status(NOT_FOUND).send({ message: (err as Error).message });
 	}
-}
+};
 
 export default getHealth;
