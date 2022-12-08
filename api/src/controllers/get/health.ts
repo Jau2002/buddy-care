08af02d7cde75bb23ec0c06b0e4e5fbf54f9a003
Asdrúbal Mejía López <asdrumejia19@gmail.com>
2022-12-08 12:25:28 -0500

    refactor (api/package.json): any removed

--- api/src/controllers/get/health.ts
+++ api/src/controllers/get/health.ts
@@ -1,10 +1,25 @@
-import { NOT_FOUND, OK } from '../protocols';
+// import { NOT_FOUND, OK } from '../protocols';
 
-function getHealth(_: any, res: any): any {
+// function getHealth(_: any, res: any): any {
+// 	console.log(getHealth)
+// 	try {
+// 		return res.status(OK).send({ message: 'the server is running faster' });
+// 	} catch (err: any) {
+// 		return res.status(NOT_FOUND).send({ message: err.message });
+// 	}
+// }
+
+// export default getHealth;
+
+import { NOT_FOUND, OK } from '../protocols';
+import {Request, Response} from 'express'
+
+const getHealth = async (req: Request, res: Response):Promise<unknown> => {
+	console.log(req)
 	try {
-		return res.status(OK).send({ message: 'the server is running faster' });
-	} catch (err: any) {
-		return res.status(NOT_FOUND).send({ message: err.message });
+		return res.status(OK).send({ message: 'the server is running' });
+	} catch (err) {
+		return res.status(NOT_FOUND).send({ message: (err as Error).message });
 	}
 }
 
