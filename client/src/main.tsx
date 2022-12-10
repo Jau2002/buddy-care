35db89d6eb1f4492d52d004827b66093a0929836
Jau2002 <idamendjaume@gmail.com>
2022-12-09 23:07:55 -0500

    feat: add linter and boostrap integrated with sass

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './app/store';
+import './sass/styles.scss';
 
 axios.defaults.baseURL =
 	import.meta.env.VITE_APP_API ?? 'http://localhost:3001';
