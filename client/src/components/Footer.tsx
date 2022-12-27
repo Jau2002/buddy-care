5d61e92bfa5875c07fbf1395ab51fe9fd3cd2cde
Asdrúbal Mejía López <asdrumejia19@gmail.com>
2022-12-27 10:53:58 -0500

    style(client/src/sass) Styled footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,9 +2,53 @@ import { ReactElement } from 'react';
 
 function Footer(): ReactElement {
 	return (
-		<>
-			<h1>Contactos</h1>
-		</>
+		<div className='footer-div'>
+		  
+			<div className='footer-div--div'>
+				<div>
+					<img
+						src='/img/face_tb.png'
+						width='60'
+						height='60'
+					/>
+				</div>
+				<div>
+					<img
+						src='/img/instagram_white.png'
+						width='60'
+						height='60'
+					/>
+				</div>
+				<div>
+					<img
+						src='/img/email_tb.png'
+						width='60'
+						height='60'
+					/>
+				</div>
+				<div>
+					<img
+						src='/img/wap_tb.png'
+						width='60'
+						height='60'
+					/>
+				</div>
+				<div>
+					<img
+						src='/img/telegram_tg.png'
+						width='60'
+						height='60'
+					/>
+				</div>
+			</div>
+
+			<div>
+				<br></br>
+				<p className='footer-div--p'>
+					(C) 2022 - Proyecto final soy Henry - Grupo 10
+				</p>
+			</div>
+		</div>
 	);
 }
 
