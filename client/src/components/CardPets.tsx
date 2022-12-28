b0ce52eb909edf49d37b0bc2b922d48fb0a8adac
Asdrúbal Mejía López <asdrumejia19@gmail.com>
2022-12-28 11:00:23 -0500

    feat(client/src) cardPets component created

---
+++ client/src/components/CardPets.tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from 'react';
+
+const CardPets = ({...props}): ReactElement => {
+	return (
+		<>
+			<div>
+				<img
+					src={props.adjunto}
+					alt='Pets_image'
+				/>
+			</div>
+
+			<div>
+				<p>{props.nombre}</p>
+				<p>{props.raza}</p>
+        <p>{props.edad}</p>
+			</div>
+		</>
+	);
+};
+
+export default CardPets;
