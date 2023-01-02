0f0df6b74f071bd3a676b297dd4857b4b0165a9d
Jau2002 <idamendjaume@gmail.com>
2023-01-02 08:49:54 -0500

    feat: inputs validate sing-up

--- client/src/pages/SingUp.tsx
+++ client/src/pages/SingUp.tsx
@@ -1,13 +1,13 @@
 import type { ReactElement } from 'react';
 import Forms from '../components/Forms';
 import GoToHome from '../components/GoToHome';
-import { logIn } from '../utils/form';
+import { signUp } from '../utils/form';
 
 function SingUp(): ReactElement {
 	return (
 		<>
 			<Forms
-				form={logIn}
+				form={signUp}
 				submit='Crear'
 				title='Crea tu propia cuenta'
 			/>
